022ed4cb6a2a64b42f7cf90fc95d72f3b8be7ff6
Nawawish <hydrokidz@gmail.com>
2018-10-07 22:57:52 +0700

    Updated middlewares

--- src/app/routes/api/user.js
+++ src/app/routes/api/user.js
@@ -20,11 +20,11 @@ const middlewares2 = [withAuth, requestBodyFilter, connectDB];
 
 userRouter.get("/:id", ...middlewares1, ctrl.get);
 
-userRouter.post("/", ...middlewares2, ctrl.create);
+userRouter.post("/", requestBodyFilter, connectDB, ctrl.create);
 
-userRouter.post("/update/:id", ...middlewares2, ctrl.update);
-userRouter.put("/:id", ...middlewares2, ctrl.update);
-userRouter.patch("/:id", ...middlewares2, requestBodyFilter, ctrl.update);
+userRouter.post("/update/:id", ...middlewares1, ctrl.update);
+userRouter.put("/:id", ...middlewares1, ctrl.update);
+userRouter.patch("/:id", ...middlewares1, requestBodyFilter, ctrl.update);
 
 userRouter.post("/delete/:id", ...middlewares1, ctrl.delete);
 userRouter.delete("/:id", ...middlewares1, ctrl.delete);
