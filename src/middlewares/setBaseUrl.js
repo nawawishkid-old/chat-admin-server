b67337c431119eb3c9768ad3fb3b6e38e7af7d36
Nawawish <hydrokidz@gmail.com>
2018-10-10 12:31:03 +0700

    Added url rewrite middleware for setting app's base url

---
+++ src/middlewares/setBaseUrl.js
@@ -0,0 +1,17 @@
+const { BASE_URL } = require("../configs").app;
+
+module.exports = (req, res, next) => {
+  console.log("[MIDDLEWARE] setBaseUrl");
+
+  console.log("- original URL: ", req.url);
+  console.log("- baseUrl: ", BASE_URL);
+
+  let baseUrl = BASE_URL || "";
+
+  req.url = req.url.slice(-1) === "/" ? req.url.slice(0, -1) : req.url;
+  req.url = baseUrl + req.url;
+
+  console.log("- set URL: ", req.url);
+
+  next();
+};
