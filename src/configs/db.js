6c0217df60ed6bad09edc80e5e5bdf9be61d51bb
Nawawish <hydrokidz@gmail.com>
2018-10-13 03:17:44 +0700

    Changed each config module to be a function that set app's global variables

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,5 +1,37 @@
-exports.HOST = process.env.DATABASE_HOST || "0.0.0.0";
-exports.PORT = process.env.DATABASE_PORT || 27017;
-exports.NAME = process.env.DATABASE_NAME || "test";
-exports.USERNAME = process.env.DATABASE_USERNAME || "admin";
-exports.PASSWORD = process.env.DATABASE_PASSWORD || "adminAtTest";
+/**
+ * Set app global variables
+ *
+ * @param {Object} app Express app object (created by calling express())
+ * @return {Object} app
+ */
+module.exports = app => {
+  const { env } = require("../utils");
+  const throwIfEmpty = msg => value => {
+    if (!value) {
+      throw new Error(msg);
+    }
+
+    return value;
+  };
+  const HOST = env("CHATADMIN_DB_HOST", "0.0.0.0");
+  const PORT = env("CHATADMIN_DB_PORT", 27017);
+  const NAME = env("CHATADMIN_DB_NAME", throwIfEmpty("Required database name"));
+  const USERNAME = env(
+    "CHATADMIN_DB_USERNAME",
+    throwIfEmpty("Required database username")
+  );
+  const PASSWORD = env(
+    "CHATADMIN_DB_PASSWORD",
+    throwIfEmpty("Required database password")
+  );
+  const AUTH_SOURCE = env("CHATADMIN_DB_AUTH_SOURCE");
+
+  app.set("mongodb host", HOST);
+  app.set("mongodb port", PORT);
+  app.set("mongodb db name", NAME);
+  app.set("mongodb db username", USERNAME);
+  app.set("mongodb db password", PASSWORD);
+  app.set("mongodb auth source", AUTH_SOURCE);
+
+  return app;
+};
--- src/configs/logger.js
+++ src/configs/logger.js
@@ -1,29 +1,33 @@
-const { LOG_LEVEL, LOG_TYPE } = require("./app");
-const logTypes = LOG_TYPE.split(",");
-const isAll = LOG_TYPE === "all";
+/**
+ * Set app global variables
+ *
+ * @param {Object} app Express app object (created by calling express())
+ * @return {Object} app
+ */
+module.exports = app => {
+  const logType = app.get("log type");
+  const logList = logType.split(",");
+  const isAll = logType === "all";
+  const isIn = type => logList.includes(type);
+  const isLog = type => isAll || isIn(type);
+  const getLevel = type => (isLog(type) ? app.get("log level") : "silent");
 
-const isIn = type => logTypes.includes(type);
-const isLog = type => isAll || isIn(type);
-const getLevel = type => (isLog(type) ? LOG_LEVEL : "silent");
+  app.set("log app", {
+    LEVEL: getLevel("app"),
+    LABEL: "App"
+  });
+  app.set("log middleware", {
+    LEVEL: getLevel("middleware"),
+    LABEL: "Middleware"
+  });
+  app.set("log controller", {
+    LEVEL: getLevel("controller"),
+    LABEL: "Controller"
+  });
+  app.set("log database", {
+    LEVEL: getLevel("database"),
+    LABEL: "Database"
+  });
 
-exports.app = {
-  LEVEL: getLevel("app"),
-  LABEL: "App"
-};
-exports.middleware = {
-  LEVEL: getLevel("middleware"),
-  LABEL: "Middleware"
-};
-exports.controller = {
-  LEVEL: getLevel("controller"),
-  LABEL: "Controller"
-};
-exports.database = {
-  LEVEL: getLevel("database"),
-  LABEL: "Database"
-};
-
-exports.api = {
-  LEVEL: LOG_LEVEL,
-  LABEL: "API"
+  return app;
 };
