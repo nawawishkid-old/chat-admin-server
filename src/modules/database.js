ffcbd966d3d8a03194d5c23621fa108cab32dd60
Nawawish <hydrokidz@gmail.com>
2018-10-14 12:05:45 +0700

    Turned into Higher order function for injecting mongodb connection info for portability and testability

--- src/modules/database.js
+++ src/modules/database.js
@@ -1,48 +1,35 @@
-const mongoose = require("mongoose");
-const app = require("../init");
-const APP_ENV = app.get("env");
-const LOG_LEVEL = app.get("log level");
-const HOST = app.get("mongodb host");
-const PORT = app.get("mongodb port");
-const NAME = app.get("mongodb db name");
-const USERNAME = app.get("mongodb db username");
-const PASSWORD = app.get("mongodb db password");
-const AUTH_SOURCE = app.get("mongodb auth source");
-const logger = require("./loggers/database");
-
-exports.connect = () => {
+exports.connect = (options, callback = null) => async () => {
+  const mongoose = require("mongoose");
+  const { username, password, host, port, name, authSrc, isDebug } = options;
   const { readyState } = mongoose.connection;
-  const authSrc = AUTH_SOURCE ? `?authSource=${AUTH_SOURCE}` : "";
-  const url = `mongodb://${USERNAME}:${PASSWORD}@${HOST}:${PORT}/${NAME}${authSrc}`;
+  const authSource = authSrc ? `?authSource=${authSrc}` : "";
+  const url = `mongodb://${username}:${password}@${host}:${port}/${name}${authSource}`;
 
-  logger.debug("Trying to connect to " + url);
-
-  // If already connected.
-  if (readyState > 0 && readyState < 3) {
-    logger.debug("Already connected to %", url);
-
-    return;
-  }
-
-  if (APP_ENV === "development" && LOG_LEVEL !== "silent") {
+  if (isDebug) {
     mongoose.set("debug", true);
   }
 
-  mongoose.connect(
+  // If already connected.
+  if (readyState > 0 && readyState < 3) {
+    return mongoose.connection;
+  }
+
+  await mongoose.connect(
     url,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true },
+    callback
   );
 
-  const db = mongoose.connection;
+  const connection = mongoose.connection;
 
-  db.on("error", err => {
-    logger.debug("Connection error: " + err);
-    logger.debug("Ready state: " + mongoose.connection.readyState);
+  connection.on("error", err => {
+    console.log("Connection error: " + err);
+    console.log("Ready state: " + mongoose.connection.readyState);
   });
-  db.once("open", () => {
-    logger.debug("Connected to " + url);
-    logger.debug("Ready state: " + mongoose.connection.readyState);
+  connection.once("open", () => {
+    console.log("Connected to " + url);
+    console.log("Ready state: " + mongoose.connection.readyState);
   });
 
-  return db;
+  return connection;
 };
