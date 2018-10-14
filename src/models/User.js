ff8237f34077faf1ee059849b88a0328b8a91e80
Nawawish <hydrokidz@gmail.com>
2018-10-15 01:01:53 +0700

    Added email validation and hide __v props

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,26 @@
 const mongoose = require("mongoose");
 const passwordHash = require("password-hash");
 const { Schema } = mongoose;
+
+const validateEmail = email => {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,6})+$/;
+
+  return regex.test(email);
+};
+
 /**
  * Required input validation e.g. email, name
  */
 const schema = new Schema({
   username: { type: String, required: true, lowercase: true, unique: true },
-  // Always store username in lower case
-  name: { type: String, required: true, lowercase: true },
-  email: { type: String, required: true, lowercase: true, unique: true },
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    unique: true,
+    validate: [validateEmail, "Invalid email address pattern"]
+  },
   password: {
     type: String,
     required: true,
@@ -16,7 +28,8 @@ const schema = new Schema({
     set: value => passwordHash.generate(value)
   },
   created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+  updated_at: { type: Date, default: Date.now, set: () => Date.now() },
+  __v: { type: Number, select: false } // hide mongoose's built-in prop
 });
 
 module.exports = mongoose.model("User", schema);
