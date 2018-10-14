01a0e8ade006a711f0cedc88d57a5a11508ec0f8
Nawawish <hydrokidz@gmail.com>
2018-10-14 12:09:08 +0700

    Changed the way of calling database module 'cause its API has changed

--- tests/api/app.js
+++ tests/api/app.js
@@ -9,6 +9,22 @@ require("dotenv").config({
 const app = require("../../src/app");
 const db = require("../../src/modules/database");
 
-db.connect();
+const logLevel = app.get("log level");
+const host = app.get("mongodb host");
+const port = app.get("mongodb port");
+const name = app.get("mongodb db name");
+const username = app.get("mongodb db username");
+const password = app.get("mongodb db password");
+const authSrc = app.get("mongodb auth source");
+const isDebug = app.get("mongodb debug") === "on";
+const connection = db.connect({
+  host,
+  port,
+  name,
+  username,
+  password,
+  authSrc,
+  isDebug
+})();
 
-module.exports = app; 
+module.exports = app;
