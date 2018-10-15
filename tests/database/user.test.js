930e3642811d81f903113915736bad35df651c58
Nawawish <hydrokidz@gmail.com>
2018-10-15 20:18:30 +0700

    Finished database testing

--- tests/database/user.test.js
+++ tests/database/user.test.js
@@ -1,197 +1,183 @@
 const { should, db, models } = require("../utils");
 const { testUser } = models;
 const {
-  autoInsertCreatedAtField,
-  notInsertUnrelatedField
+  shouldAlterField,
+  shouldAutoInsertsField,
+  shouldNotInsertsIfUnrelatedField,
+  shouldInsertsValidDocument,
+  shouldNotReturnsField,
+  shouldNotInsertsIfErrorField,
+  shouldNotInsertsIfFieldIsMissing,
+  shouldNotDuplicatesUniqueField
 } = require("./cases");
-const prefix = "[DATABASE] ";
+const prefix = require("./utils").prefix + "[User]";
 const User = require("../../src/models/User");
 const userDoc = testUser.data;
 const passwordHash = require("password-hash");
 const caseOptions = { model: User, baseDoc: userDoc };
 
-describe(`${prefix}User create`, () => {
+describe(`${prefix} create`, () => {
   before(async () => {
     await db.connect();
   });
 
   after(async () => {
-    await User.remove({});
+    await db.reset();
     db.disconnect();
   });
 
-  beforeEach(async () => await User.remove({}));
-
-  it("should inserts new document which has the same fields of given valid data object", async () => {
-    await User.create({ ...userDoc }, (err, doc) => {
-      should.not.exist(err);
-      should.exist(doc);
-
-      Object.keys(userDoc).forEach(key => {
-        doc.should.have.property(key); //, userDoc[key]);
-      });
-    });
-  });
-
-  it("should hashes user password", async () => {
-    await User.create({ ...userDoc }, (err, doc) => {
-      should.not.exist(err);
-      should.exist(doc);
-      passwordHash.verify(userDoc.password, doc.password).should.be.true;
-    });
-  });
-
-  it("should lowercase username", async () => {
-    await User.create({ ...userDoc }, (err, doc) => {
-      should.not.exist(err);
-      should.exist(doc);
-      doc.should.have.property("username", userDoc.username.toLowerCase());
-      // passwordHash.verify(userDoc.password, doc.password).should.be.true;
-
-      // delete userDoc.password;
-
-      // Object.keys(userDoc).forEach(key => {
-      //   doc.should.have.property(key); //, userDoc[key]);
-      // });
-    });
-  });
-
-  it("should lowercase email", async () => {
-    await User.create({ ...userDoc }, (err, doc) => {
-      should.not.exist(err);
-      should.exist(doc);
-      doc.should.have.property("email", userDoc.email.toLowerCase());
-      // passwordHash.verify(userDoc.password, doc.password).should.be.true;
-
-      // delete userDoc.password;
-
-      // Object.keys(userDoc).forEach(key => {
-      //   doc.should.have.property(key); //, userDoc[key]);
-      // });
-    });
-  });
+  beforeEach(async () => await db.reset());
 
   it(
-    autoInsertCreatedAtField.it,
-    autoInsertCreatedAtField.getCallback(caseOptions)
+    shouldInsertsValidDocument.getName(),
+    shouldInsertsValidDocument.getCallback(caseOptions)
   );
 
-  it("should not inserts if email is an invalid email address pattern", async () => {
-    const newDoc = { ...userDoc, email: "invalid@email" };
-
-    await User.create(newDoc).catch(err => {
-      should.exist(err.errors.email);
-    });
-  });
-
-  it("should not inserts if 'username' field is missing", async () => {
-    const newDoc = { ...userDoc };
-
-    delete newDoc.username;
-
-    await User.create(newDoc).catch(err => {
-      should.exist(err.errors.username);
-    });
-  });
-
-  it("should not inserts if 'name' field is missing", async () => {
-    const newDoc = { ...userDoc };
-
-    delete newDoc.name;
-
-    await User.create(newDoc).catch(err => {
-      should.exist(err.errors.name);
-    });
-  });
-
-  it("should not inserts if 'password' field is missing", async () => {
-    const newDoc = { ...userDoc };
-
-    delete newDoc.password;
-
-    await User.create(newDoc).catch(err => {
-      should.exist(err.errors.password);
-    });
-  });
-
-  it("should not inserts if 'email' field is missing", async () => {
-    const newDoc = { ...userDoc };
-
-    delete newDoc.email;
-
-    await User.create(newDoc).catch(err => {
-      should.exist(err.errors.email);
-    });
-  });
-
-  it("should not duplicates document with unique 'username'", async () => {
-    const newDoc = { ...userDoc, email: "not@duplicate.field" };
-
-    await User.create(userDoc);
-    await User.create(newDoc).catch(err => {
-      should.exist(err);
-      err.should.have.property("code", 11000);
-    });
-  });
-
-  it("should not duplicates document with unique 'email'", async () => {
-    const newDoc = { ...userDoc, username: "notduplicate" };
-
-    await User.create(userDoc);
-    await User.create(newDoc).catch(err => {
-      should.exist(err);
-      err.should.have.property("code", 11000);
-    });
-  });
+  it(
+    shouldAlterField.getName("hashes", "password"),
+    shouldAlterField.getCallback({
+      ...caseOptions,
+      field: "password",
+      callback: (newValue, oldValue) =>
+        passwordHash.verify(oldValue, newValue) ? newValue : oldValue
+    })
+  );
 
   it(
-    notInsertUnrelatedField.it,
-    notInsertUnrelatedField.getCallback(caseOptions)
+    shouldAlterField.getName("lowercase", "username"),
+    shouldAlterField.getCallback({
+      ...caseOptions,
+      field: "username",
+      callback: value => value.toLowerCase()
+    })
+  );
+
+  it(
+    shouldAlterField.getName("lowercase", "email"),
+    shouldAlterField.getCallback({
+      ...caseOptions,
+      field: "email",
+      callback: value => value.toLowerCase()
+    })
+  );
+
+  it(
+    shouldAutoInsertsField.getName("created_at"),
+    shouldAutoInsertsField.getCallback({
+      ...caseOptions,
+      field: "created_at",
+      fieldType: "Date"
+    })
+  );
+
+  it(
+    shouldNotInsertsIfErrorField.getName(
+      "'email' is an invalid email address pattern"
+    ),
+    shouldNotInsertsIfErrorField.getCallback({
+      ...caseOptions,
+      baseDoc: { ...caseOptions.baseDoc, email: "invalid@email" },
+      field: "email"
+    })
+  );
+
+  it(
+    shouldNotInsertsIfFieldIsMissing.getName("username"),
+    shouldNotInsertsIfFieldIsMissing.getCallback({
+      ...caseOptions,
+      field: "username"
+    })
+  );
+
+  it(
+    shouldNotInsertsIfFieldIsMissing.getName("name"),
+    shouldNotInsertsIfFieldIsMissing.getCallback({
+      ...caseOptions,
+      field: "name"
+    })
+  );
+
+  it(
+    shouldNotInsertsIfFieldIsMissing.getName("password"),
+    shouldNotInsertsIfFieldIsMissing.getCallback({
+      ...caseOptions,
+      field: "password"
+    })
+  );
+
+  it(
+    shouldNotInsertsIfFieldIsMissing.getName("email"),
+    shouldNotInsertsIfFieldIsMissing.getCallback({
+      ...caseOptions,
+      field: "email"
+    })
+  );
+
+  it(
+    shouldNotDuplicatesUniqueField.getName("username"),
+    shouldNotDuplicatesUniqueField.getCallback({
+      ...caseOptions,
+      email: "not@duplicate.com"
+    })
+  );
+
+  it(
+    shouldNotDuplicatesUniqueField.getName("email"),
+    shouldNotDuplicatesUniqueField.getCallback({
+      ...caseOptions,
+      username: "notduplicate"
+    })
+  );
+
+  it(
+    shouldNotInsertsIfUnrelatedField.getName(),
+    shouldNotInsertsIfUnrelatedField.getCallback(caseOptions)
   );
 });
 
-describe(`${prefix}User get`, () => {
+describe(`${prefix} get`, () => {
+  let userId;
+
   before(async () => {
     await db.connect();
+
+    userId = await User.create(userDoc).then(doc => doc._id);
   });
 
   after(async () => {
-    await User.remove({});
+    await db.reset();
+
     db.disconnect();
   });
 
-  beforeEach(async () => await User.remove({}));
-
-  it("should not returns 'password' field by default", async () => {
-    const userId = await User.create(userDoc).then(doc => doc._id);
-
-    await User.findOne({ _id: userId }).then(doc => {
-      should.exist(doc);
-      should.not.exist(doc.password);
+  it(shouldNotReturnsField.getName("password"), async () => {
+    await shouldNotReturnsField.getCallback({
+      ...caseOptions,
+      _id: userId,
+      field: "password"
     });
   });
 
-  it("should not returns '__v' built-in field by default", async () => {
-    const userId = await User.create(userDoc).then(doc => doc._id);
-
-    await User.findOne({ _id: userId }).then(doc => {
-      should.exist(doc);
-      should.not.exist(doc.__v);
-    });
+  it(shouldNotReturnsField.getName("__v"), async () => {
+    await shouldNotReturnsField.getCallback({
+      ...caseOptions,
+      _id: userId,
+      field: "__v"
+    })();
   });
 });
 
-describe(`${prefix}User update`, () => {
+describe(`${prefix} update`, () => {
   before(async () => {
     await db.connect();
   });
 
   after(async () => {
-    await User.remove({});
+    await db.reset();
     db.disconnect();
   });
 
-  beforeEach(async () => await User.remove({}));
+  beforeEach(async () => await db.reset());
 
   xit("should automatically updates 'updated_at' field on updating existing document", async () => {
     const createdDoc = await User.create(userDoc).then(doc => doc);
@@ -208,17 +194,17 @@ describe(`${prefix}User update`, () => {
   });
 });
 
-describe(`${prefix}User remove`, () => {
+describe(`${prefix} remove`, () => {
   before(async () => {
     await db.connect();
   });
 
   after(async () => {
-    await User.remove({});
+    await db.reset();
     db.disconnect();
   });
 
-  beforeEach(async () => await User.remove({}));
+  beforeEach(async () => await db.reset());
 
   it("should remove user", async () => {
     const { _id } = await User.create(userDoc).then(doc => doc);
