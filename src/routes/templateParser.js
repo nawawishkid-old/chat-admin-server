f7fc265314244b3e1a79c92b3a9222b7b4a899ee
Nawawish Samerpark <hydrokidz@gmail.com>
2018-11-09 16:21:19 +0700

    fixed: correct template parser route's middleware

--- src/routes/templateParser.js
+++ src/routes/templateParser.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
 const ctrl = require("../controllers/templateParser");
-const { connectDB } = require("../middlewares");
+const { connectDB, withCreatorId } = require("../middlewares");
 const { withAuth } = require("./middlewares");
 const router = Router();
 
 // Get
-router.get("/:templateId", withAuth, connectDB, ctrl.get);
+router.get("/:templateId", withCreatorId, withAuth, connectDB, ctrl.get);
 
 module.exports = router;
